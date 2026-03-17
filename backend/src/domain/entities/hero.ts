ee90e8587d4256896179f93440c1c8c0c14e6e42
Leo <leonardonabarro2003@gmail.com>
2026-03-17 16:25:00 -0300

    feat: add hero entity

---
+++ backend/src/domain/entities/hero.ts
@@ -0,0 +1,12 @@
+export type Hero = {
+    id: string;
+    name: string;
+    nickname: string;
+    date_of_birth: Date;
+    universe: string;
+    main_power: string;
+    avatar_url: string;
+    is_active: boolean;
+    created_at: Date;
+    updated_at: Date;
+};
